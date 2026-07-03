8cdd7a833bcfc29211c51e60a161f68fb8757f97
iki1uc <entertrainer@web.de>
2026-07-03 20:46:12 +0200

    Update cpu_router.js

--- cpu_router.js
+++ cpu_router.js
@@ -1,1 +1,19 @@
+export function CPU_ROUTER(msg) {
+    const text = String(msg || "");
+
+    // GPU
+    if (text.startsWith("GPU-")) return { sys: "GPU", num: text };
+
+    // CPU
+    if (text.startsWith("CPU-")) return { sys: "CPU", num: text };
+
+    // RAM
+    if (text.startsWith("RAM-")) return { sys: "RAM", num: text };
+
+    // ROM
+    if (text.startsWith("ROM-")) return { sys: "ROM", num: text };
+
+    // Fehler
+    return { sys: "AUA", num: 99 };
+}
 
